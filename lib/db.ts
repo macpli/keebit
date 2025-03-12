c2ed3bc5ff71354050b6fc849a1906abf002ffe7
macpli <pliszekmaciej0@gmail.com>
2025-03-12 18:11:10 +0100

    fix: hardcoded db port

--- lib/db.ts
+++ lib/db.ts
@@ -11,7 +11,7 @@ export const pool =
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    port: 5432,
+    port: process.env.DATABASE_PORT,
   })
 
 // W trybie development zapisujemy pool w zmiennej globalnej
