e12e82f8a0048f670d043ed3236b82e5cddde920
macpli <pliszekmaciej0@gmail.com>
2025-03-12 17:52:16 +0100

    change eslint temp

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "no-console": "off",
+      "react/jsx-uses-react": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
